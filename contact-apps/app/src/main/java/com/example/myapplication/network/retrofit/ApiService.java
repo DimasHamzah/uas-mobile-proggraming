1879fb35f533a0b80e6ecb6e3fdf11ee85f0ee8a
DimasHamzah <hamzahdimas85@gmail.com>
2022-06-28 13:27:37 +0800

    add method retroft part

--- contact-apps/app/src/main/java/com/example/myapplication/network/retrofit/ApiService.java
+++ contact-apps/app/src/main/java/com/example/myapplication/network/retrofit/ApiService.java
@@ -4,6 +4,7 @@ import com.example.myapplication.network.response.ResponseDetail;
 import com.example.myapplication.network.response.ResponseListContact;
 
 import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.DELETE;
 import retrofit2.http.Field;
@@ -24,26 +25,27 @@ public interface ApiService {
   @Multipart
   @POST("contact")
   Call<ResponseDetail> requestPostContact(
-    @Part("image") MultipartBody.Part image,
-    @Field("username") String username,
-    @Field("alamat") String alamat,
-    @Field("notelphone") String notelphone,
-    @Field("email") String email,
-    @Field("tanggalLahir") String tanggalLahir,
-    @Field("jenisKelasmin") String jenisKelamin
+    @Part("file") MultipartBody.Part file,
+    @Part("username")RequestBody username,
+    @Part("alamat") RequestBody alamat,
+    @Part("notelphone") RequestBody notelphone,
+    @Part("email") RequestBody email,
+    @Part("tanggalLahir") RequestBody tanggalLahir,
+    @Part("jenisKelasmin") RequestBody jenisKelamin
     );
 
   @Multipart
   @PUT("contact/{id}")
   Call<ResponseDetail> requestUpdateContact(
     @Path("id") Integer id,
-    @Part("image") MultipartBody.Part image,
-    @Field("username") String username,
-    @Field("alamat") String alamat,
-    @Field("notelphone") String notelphone,
-    @Field("email") String email,
-    @Field("tanggalLahir") String tanggalLahir,
-    @Field("jenisKelasmin") String jenisKelamin );
+    @Part("file") MultipartBody.Part file,
+    @Part("username")RequestBody username,
+    @Part("alamat") RequestBody alamat,
+    @Part("notelphone") RequestBody notelphone,
+    @Part("email") RequestBody email,
+    @Part("tanggalLahir") RequestBody tanggalLahir,
+    @Part("jenisKelasmin") RequestBody jenisKelamin
+  );
 
   @DELETE("contact/{id}")
   Call<ResponseDetail> requestDeleteContact(@Path("id") Integer id);
