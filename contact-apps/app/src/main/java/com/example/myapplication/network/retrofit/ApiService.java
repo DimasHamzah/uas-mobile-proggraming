1ac4354b592a5458ee5cde1ab5374b73ed53d1ec
DimasHamzah <hamzahdimas85@gmail.com>
2022-06-28 13:22:02 +0800

    add method retrofi

--- contact-apps/app/src/main/java/com/example/myapplication/network/retrofit/ApiService.java
+++ contact-apps/app/src/main/java/com/example/myapplication/network/retrofit/ApiService.java
@@ -3,11 +3,15 @@ package com.example.myapplication.network.retrofit;
 import com.example.myapplication.network.response.ResponseDetail;
 import com.example.myapplication.network.response.ResponseListContact;
 
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.DELETE;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface ApiService {
@@ -17,11 +21,29 @@ public interface ApiService {
   @GET("contact/{id}")
   Call<ResponseDetail> requestGetDetailContact(@Path("id") Integer id);
 
+  @Multipart
   @POST("contact")
-  Call<ResponseDetail> requestPostContact();
+  Call<ResponseDetail> requestPostContact(
+    @Part("image") MultipartBody.Part image,
+    @Field("username") String username,
+    @Field("alamat") String alamat,
+    @Field("notelphone") String notelphone,
+    @Field("email") String email,
+    @Field("tanggalLahir") String tanggalLahir,
+    @Field("jenisKelasmin") String jenisKelamin
+    );
 
+  @Multipart
   @PUT("contact/{id}")
-  Call<ResponseDetail> requestUpdateContact(@Path("id") Integer id);
+  Call<ResponseDetail> requestUpdateContact(
+    @Path("id") Integer id,
+    @Part("image") MultipartBody.Part image,
+    @Field("username") String username,
+    @Field("alamat") String alamat,
+    @Field("notelphone") String notelphone,
+    @Field("email") String email,
+    @Field("tanggalLahir") String tanggalLahir,
+    @Field("jenisKelasmin") String jenisKelamin );
 
   @DELETE("contact/{id}")
   Call<ResponseDetail> requestDeleteContact(@Path("id") Integer id);
